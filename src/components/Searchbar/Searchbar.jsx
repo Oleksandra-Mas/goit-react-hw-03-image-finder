3e38fb69996ad861943a6496f0eb8fb5363a28aa
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2021-12-11 23:31:19 +0200

    added apiService

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+function Searchbar(props) {
+    return null;
+}
+// Profile.propTypes = {
+//     username: PropTypes.string.isRequired,
+//     tag: PropTypes.string.isRequired,
+//     location: PropTypes.string.isRequired,
+//     avatar: PropTypes.string.isRequired,
+//     stats: PropTypes.shape({
+//         followers: PropTypes.number.isRequired,
+//         views: PropTypes.number.isRequired,
+//         likes: PropTypes.number.isRequired,
+//     }).isRequired,
+// };
+export default Searchbar;
---
+++ src/services/apiService.js
@@ -0,0 +1,37 @@
+const BASE_URL = "https://pixabay.com/api";
+const API_KEY = "23883543-c0e8a740f16e18e27aeb57e6b";
+export default class ApiService {
+  constructor() {
+    this.searchQuery = "";
+    this.page = 1;
+  }
+  async searchPhoto() {
+    const url = `${BASE_URL}/?q=${this.searchQuery}&page=${this.page}key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+
+  decrementPage() {
+    this.page -= 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  setPage(page) {
+    this.page = page;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
